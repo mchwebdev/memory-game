9a1b8e6279c7cdaa30e602c23ee6b27abf38506a
mchwebdev <36421426+mchwebdev@users.noreply.github.com>
2018-03-25 19:20:21 +0200

    add utils object to store shuffle and startTimer functions

--- js/app.js
+++ js/app.js
@@ -1,12 +1,16 @@
+// Variable declarations
 // Array of cards
 const cards = ['fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt', 'fa-cube', 'fa-leaf', 'fa-bicycle', 'fa-bomb'];
 const doubledCards = cards.concat(cards);
 let openCards = [];
+
+// Utils stores shuffle and startTimer functions
+const utils = {};
 const game = {
 	moves: 0,
 	matchesFound: 0,
 	starRating: 3
-}
+};
 let timer;
 let sec = 0;
 const seconds = document.querySelectorAll('.seconds');
@@ -14,7 +18,7 @@ const minutes = document.querySelectorAll('.minutes');
 const card = document.getElementsByClassName('card');
 
 // Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
+utils.shuffle = function(array) {
 	var currentIndex = array.length,
 		temporaryValue, randomIndex;
 	while (currentIndex !== 0) {
@@ -25,11 +29,33 @@ function shuffle(array) {
 		array[randomIndex] = temporaryValue;
 	}
 	return array;
-}
+};
+
+// Function to start the timer on the first click
+utils.startTimer = function() {
+	// Remove the event listener to prevent timer to trigger more than once
+	for (let i = 0; i < card.length; i++) {
+		card[i].removeEventListener('click', utils.startTimer);
+	}
+
+	function time(val) {
+		return val > 9 ? val : '0' + val;
+	}
+	timer = setInterval(function () {
+
+		// Updates the innerText of the timer during playing
+		seconds[0].innerText = time(++sec % 60);
+		minutes[0].innerText = time(parseInt(sec / 60, 10));
+
+		// Updates the innerText of the popup when the game is won
+		seconds[1].innerText = seconds[0].innerText;
+		minutes[1].innerText = minutes[0].innerText;
+	}, 1000);
+};
 
 // Create a new board adding html to the DOM
 function newBoard() {
-	const shuffledDeck = shuffle(doubledCards);
+	const shuffledDeck = utils.shuffle(doubledCards);
 	let output = '';
 	const deck = document.querySelector('.deck');
 	for (let i = 0; i < shuffledDeck.length; i++) {
@@ -167,28 +193,6 @@ function popupWinner() {
 	}
 }
 
-// Function to start the timer on the first click
-function startTimer() {
-	// Remove the event listener to prevent timer to trigger more than once
-	for (let i = 0; i < card.length; i++) {
-		card[i].removeEventListener('click', startTimer);
-	}
-
-	function time(val) {
-		return val > 9 ? val : '0' + val;
-	}
-	timer = setInterval(function () {
-
-		// Updates the innerText of the timer during playing
-		seconds[0].innerText = time(++sec % 60);
-		minutes[0].innerText = time(parseInt(sec / 60, 10));
-
-		// Updates the innerText of the popup when the game is won
-		seconds[1].innerText = seconds[0].innerText;
-		minutes[1].innerText = minutes[0].innerText;
-	}, 1000);
-}
-
 // Function to restart the game when icon is clicked
 function restartGame() {
 	const restart = document.querySelector('.restart');
@@ -202,5 +206,5 @@ restartGame();
 
 // Click event to call the startTimer function when first card is clicked
 for (let i = 0; i < card.length; i++) {
-	card[i].addEventListener('click', startTimer);
+	card[i].addEventListener('click', utils.startTimer);
 }
